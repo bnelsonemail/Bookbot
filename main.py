d20d8202364cfca44060b9e14439ae1dfd69b405
Brice Nelson <bnelsonemail@icloud.com>
2025-03-24 01:23:34 -0400

    refactored main.py to add in book report.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, char_count, sort_char_dict
 
 
 def get_book_text(file_path):
@@ -7,12 +7,26 @@ def get_book_text(file_path):
         return f.read()
 
 
+def print_report(file_path, word_total, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_total} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
+
+
 def main():
-    """Reads book content from a file and prints word count."""
-    file_content = get_book_text('books/frankenstein.txt')
-    word_count(file_content)
-    return file_content
+    path = 'books/frankenstein.txt'
+    book = get_book_text(path)
+    word_total, cleaned_text = word_count(book)
+    char_dict = char_count(cleaned_text)
+    sorted_chars = sort_char_dict(char_dict)
+    print_report(path, word_total, sorted_chars)
 
 
 if __name__ == "__main__":
     main()
+
